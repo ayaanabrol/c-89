f32026d64010dec1a0602a0855085a2ab2365202
ayaanabrol <69078639+ayaanabrol@users.noreply.github.com>
2021-08-27 16:09:54 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig={
+      apiKey: "AIzaSyC4aijKP4yMBCX4bjECa1CO81bS7I29SKk",
+    authDomain: "storytelling-app-787b9.firebaseapp.com",
+    projectId: "storytelling-app-787b9",
+    storageBucket: "storytelling-app-787b9.appspot.com",
+    messagingSenderId: "479426503325",
+    appId: "1:479426503325:web:2eab0095e00f2ade24aa74"
+}
